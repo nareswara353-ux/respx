e23b0e529e68a8d9eccca084850ba6e81ede4e19
Narezzzs <nareswara353@gmail.com>
2026-09-09 15:51:57 +0700

    feat(intset): add integer set header with variable encoding

--- include/kave/intset.h
+++ include/kave/intset.h
@@ -5,23 +5,21 @@
 #include <stdint.h>
 
 typedef struct intset {
+    uint32_t encoding;
     uint32_t length;
-    uint32_t alloc;
-    uint8_t encoding;
-    uint8_t *contents;
+    void *contents;
 } intset;
-
-#define INTSET_ENC_INT16 0
-#define INTSET_ENC_INT32 1
-#define INTSET_ENC_INT64 2
 
 intset *intset_new(void);
 void intset_free(intset *is);
-int intset_add(intset *is, int64_t value);
-int intset_remove(intset *is, int64_t value);
-int intset_contains(const intset *is, int64_t value);
-size_t intset_size(const intset *is);
-int64_t intset_get(const intset *is, size_t index);
-void intset_foreach(const intset *is, void (*callback)(int64_t value, void *user), void *user);
+intset *intset_add(intset *is, int64_t value);
+intset *intset_remove(intset *is, int64_t value);
+int intset_find(const intset *is, int64_t value);
+int64_t intset_random(const intset *is);
+uint32_t intset_len(const intset *is);
+intset *intset_union(const intset *a, const intset *b);
+intset *intset_intersection(const intset *a, const intset *b);
+intset *intset_difference(const intset *a, const intset *b);
+void intset_foreach(const intset *is, void (*fn)(int64_t value, void *user), void *user);
 
 #endif
