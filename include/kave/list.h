4a7c07476520cca4b6676f84f212bdebdf7edf2a
Narezzzs <nareswara353@gmail.com>
2026-09-09 15:47:37 +0700

    feat(list): add doubly-linked list header

--- include/kave/list.h
+++ include/kave/list.h
@@ -4,33 +4,31 @@
 #include <stddef.h>
 
 typedef struct list_node {
-    void *data;
     struct list_node *prev;
     struct list_node *next;
+    void *data;
 } list_node;
 
 typedef struct list {
     list_node *head;
     list_node *tail;
     size_t length;
-    void (*free_data)(void *);
 } list;
 
-list *list_new(void (*free_data)(void *));
+list *list_new(void);
 void list_free(list *l);
-int list_push_head(list *l, void *data);
-int list_push_tail(list *l, void *data);
+list_node *list_node_new(void *data);
+list *list_append(list *l, void *data);
+list *list_prepend(list *l, void *data);
+list *list_insert_before(list *l, list_node *node, void *data);
+list *list_insert_after(list *l, list_node *node, void *data);
 void *list_pop_head(list *l);
 void *list_pop_tail(list *l);
-int list_insert_before(list *l, list_node *node, void *data);
-int list_insert_after(list *l, list_node *node, void *data);
-int list_delete(list *l, list_node *node);
+int list_delete_node(list *l, list_node *node);
 list_node *list_find(const list *l, const void *data, int (*cmp)(const void *, const void *));
+void list_foreach(const list *l, void (*fn)(void *data, void *user), void *user);
+size_t list_length(const list *l);
 list_node *list_head(const list *l);
 list_node *list_tail(const list *l);
-list_node *list_next(const list_node *node);
-list_node *list_prev(const list_node *node);
-size_t list_length(const list *l);
-void list_foreach(const list *l, void (*callback)(void *data, void *userdata), void *userdata);
 
 #endif
